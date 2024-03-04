f5d59bf5ef5a6ff9da74a937aa00c5cb497fe7b1
LucarioKid <91693995+LucarioKid@users.noreply.github.com>
2024-03-04 11:36:53 -0500

    Began work on movement relative to camera

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerController : MonoBehaviour
+{
+    // Serialized variables
+    [SerializeField] private float _movementSpeed = 5f;
+    [SerializeField] private float _jumpHeight;
+    [SerializeField] private GameObject _camFocus;
+    
+    // Basic movement variables
+    private Vector3 _movementInput;
+    private Vector3 _forward;
+    private Vector3 _right;
+    private Rigidbody _rigidBody;
+    private Vector3 _camForward;
+    private Vector3 _camRight;
+    
+    // To manage jumping (and not jumping infinitely)
+    private bool _isJumping;
+    private bool _isMidair = true;
+    
+    // Input system
+    private PlayerInput _playerInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _movementInput = _playerInput.actions["Movement"].ReadValue<Vector2>();
+        
+        //Debug.Log(_movementInput);
+    }
+
+    void FixedUpdate()
+    {
+        _camForward = _camFocus.transform.forward - new Vector3(0, _camFocus.transform.forward.y, 0);
+        _camRight = _camFocus.transform.right - new Vector3(0, _camFocus.transform.right.y, 0);
+        _camForward.Normalize();
+        _camRight.Normalize();
+        
+        _forward = _movementInput.normalized.z * _movementSpeed * _camForward;
+        _right = _movementInput.normalized.x * _movementSpeed * _camRight;
+        
+        if ((_forward + _right).magnitude > .1)
+        {
+            _rigidBody.velocity = _forward + _right + new Vector3(0, _rigidBody.velocity.y, 0);
+        }
+    }
+}
+
