cf4c33b46e2b26cae258cb7cf33ed8261a79fc8e
jaysenhoward <jhoward33@fingerlakes.edu>
2024-03-08 13:13:18 -0500

    ToggleInventorySetup

--- Assets/Scripts/InventoryAndPickups/InventoryManager.cs
+++ Assets/Scripts/InventoryAndPickups/InventoryManager.cs
@@ -1,17 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
   public InventorySlot[] _inventorySlots;
   public GameObject inventoryItemPrefab;
+  public GameObject inventory;
+  public PlayerControls input;
 
+  private InputAction toggleInventory;
   private int selectedSlot = -1;
 
- 
+  void Awake()
+  {
+    input = new PlayerControls();
+  }
 
-  
+  private void OnEnable()
+  {
+    toggleInventory = input.Player.Inventory;
+    toggleInventory.Enable();
+    toggleInventory.performed += toggleInv;
+  }
+
+  private void OnDisable()
+  {
+    toggleInventory.Disable();
+  }
+
+
   public void changeSelectedSlot(int slotNum)
   {
     if (selectedSlot >= 0)
@@ -20,10 +40,23 @@ public class InventoryManager : MonoBehaviour
     }
 
     _inventorySlots[slotNum].Select();
-      selectedSlot = slotNum;
-    
+    selectedSlot = slotNum;
+
+
   }
-  
+
+  public void toggleInv(InputAction.CallbackContext context)
+  {
+    if (inventory.activeSelf == isActiveAndEnabled)
+    {
+      inventory.SetActive(false);
+    }
+    else
+    {
+      inventory.SetActive(true);
+    }
+  }
+
   public bool AddItem(Item item)
   {
     for (int i = 0; i < _inventorySlots.Length; i++)
