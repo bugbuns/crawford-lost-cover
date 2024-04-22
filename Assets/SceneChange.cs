186031ad3d1c77c00c91e7a3d9c75d9e95b943d5
acampbell34 <mothbugbuns@gmail.com>
2024-04-22 14:42:20 -0400

    intro

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement; 
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    public float changeTime;
+    public string sceneName;
+
+    public void Update()
+    {
+        changeTime -= Time.deltaTime;
+        if(changeTime <= 0)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
