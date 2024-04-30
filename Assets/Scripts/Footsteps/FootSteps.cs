af9d7d41f19feb1abd0f36112e990d2c885cbfe2
acampbell34 <mothbugbuns@gmail.com>
2024-04-30 03:14:42 -0400

    added footsteps sounds (only for normal walking)

---
+++ Assets/Scripts/Footsteps/FootSteps.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] Carpet;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Step()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return Carpet[UnityEngine.Random.Range(0, Carpet.Length)];
+        
+    }
+}
