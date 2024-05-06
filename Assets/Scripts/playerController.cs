d2958b319e93208e855952178719cfb7447fb6bb
acampbell34 <mothbugbuns@gmail.com>
2024-05-05 20:24:21 -0400

    added load into and kitchen models

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.Animations;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Animations;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
